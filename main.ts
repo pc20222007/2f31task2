d4a0ebc18a97b8430fe396bc61195fd04dd755bb
pc20222007 <115199002+pc20222007@users.noreply.github.com>
2022-10-07 09:44:47 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,84 @@
+input.onButtonPressed(Button.A, function () {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.pause(1000)
+})
+input.onButtonPressed(Button.B, function () {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . . #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . # .
+        . . . # #
+        . . . . #
+        . . . . #
+        `)
+    basic.showLeds(`
+        . . . # #
+        . . # . #
+        . . # # #
+        . . . # #
+        . . . # #
+        `)
+    basic.showLeds(`
+        . . # # #
+        . # . # .
+        . # # # #
+        . . # # #
+        . . # # #
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . # . #
+        # # # # #
+        . # # # .
+        . # # # .
+        `)
+})
